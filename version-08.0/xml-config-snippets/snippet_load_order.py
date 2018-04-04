4bebf9ebad2dade656e159ed2d575617bcb21ed1
scottshoaf <sshoaf@paloaltonetworks.com>
2018-04-04 14:13:15 -0400

    added xpaths list and xml load order python files

---
+++ version-08.0/xml-config-snippets/snippet_load_order.py
@@ -0,0 +1,18 @@
+# list of services
+
+gold_template_dict = OrderedDict([
+      ('device_group', ['devicegroup']),
+      ('profile', [ "virus", "spyware", "vulnerability", "file-blocking", "url-filtering", "wildfire-analysis", "custom-url-category", "decryption"]),
+      ('profile_group_all', ['profile_group']),
+      ('address', ['address']),
+      ('tag', ['tag']),
+      ('external_list', ['external_list']),
+      ('template', ['template']),
+      ('update_schedule', ['update-schedule']),
+      ('log_profile', ['log-profile']),
+      ('security_rules_pre', ['security-rules-gold']),
+      ('default_security_rules', ['default-security-rules']),
+      ('zone_protection_profile', ['zone_protection_profile']),
+      ('wildfire', ['wildfire']),
+      ('ntp-servers', ['ntp-servers']),
+                    ])
