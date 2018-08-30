bba29de6f264e454fdb6131cea7007ba63330ae9
scottshoaf <sshoaf@paloaltonetworks.com>
2018-08-30 15:54:11 -0400

    added elements to device-system for mgmt intf type = dhcp-client to fix commit error

--- tools/my_variables.py
+++ tools/my_variables.py
@@ -31,7 +31,7 @@ xmlvar = {
     "ADMINISTRATOR_USERNAME": "iron-skillet",
     "ADMINISTRATOR_PASSWORD": "fortheloveofallthingsholychangeme",
     # MY_CONFIGDIR is the prefix to the my_template output folder
-    "MYCONFIG_DIR": "sample-my_config",
+    "MYCONFIG_DIR": "my_local_config",
     # MGMT_TYPE values: static, dhcp-cloud, or dhcp-client
     # if static, update the IP, mask, gateway values below
     "MGMT_TYPE": "dhcp-client",
@@ -40,7 +40,7 @@ xmlvar = {
     "PANORAMA_TYPE": "standard",
     # the values below are specific to the firewall deployment environment or default can be used
     # IP addresses are non-routable in the sample config
-    "FW_NAME": "sample",
+    "FW_NAME": "firewall",
     "DEVICE_GROUP": "sample",
     "TEMPLATE": "sample",
     "DNS_1": "8.8.8.8",
